afbc8409f08cb4ec11f7d478b5fe04ca0b2c818a
Seval Künler <knlr.swl@hotmail.com>
2021-09-16 21:37:30 +0300

    ödev tamamlandı

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Ciceksepeti-Odev-1
+
+Bu proje Çiçeksepeti Bootcamp proğramının ilk projesidir. 
+
+Projede Html, Css ve JavaScript kullanılmıştır. 
+
+Css'de form ve card yapısı kullanıldı. Ayrıca card yapısında grid sistem kullanıldı. 
+
+JavaScript'te Fetch ile Json formatında 10 adet veri çekildi ve bu veriler card yapısında gösterildi.
+
+
+	
+<img
+            class="img-readme"
+            src="img/Anasayfa.png"
+            alt=""
+            srcset=""
+          />
+<img
+            class="img"
+            src="img/Form.png"
+            alt=""
+            srcset=""
+          />
+<img
+            class="img"
+            src="img/Galeri.png"
+            alt=""
+            srcset=""
+          />
---
+++ script.js
@@ -0,0 +1,58 @@
+function toggleLeftSide(){
+    document.getElementById('leftSide').classList.toggle('active')
+}
+
+const form = document.getElementsByClassName('form-control');
+addEventListener('submit', function(event){
+
+
+    event.preventDefault();
+
+    const name = document.getElementById("name").value
+    console.log(`Ad: ${name}`)
+    const surname = document.getElementById("surname").value
+    console.log(`Soyad: ${surname}`)
+    const age = document.getElementById("age").value
+    console.log(`Yaş: ${age}`)
+    const city = document.getElementById("city").value
+    console.log(`İl: ${city}`)
+    const phone = document.getElementById("phone").value
+    console.log(`Telefon No: ${phone}`)
+    
+    
+})
+const cardBody = document.querySelector('.card-body')
+console.log(cardBody)
+// let allData=[]
+fetch( 'https://jsonplaceholder.typicode.com/posts')
+.then(response=>response.json())
+.then(data=>{
+    let allData=[]
+    // const newData=data.map((value,index)=>{
+    //     if(index<10){
+    //         allData.push(value)
+    //     }
+    // }),
+    const newData=data.slice(0,10)
+
+    card(newData)
+})
+
+const card =(data) =>{
+    console.log(data)
+  data.forEach(element => {
+    const card=`<div class="card">
+    <div class="card-image"></div>
+    <div class="card-text" id="cardText">
+      <h2>${element.title.slice(0-10)}</h2>
+      <p>
+      ${element.body}
+      </p>
+    </div>
+    
+  </div>`
+  cardBody.innerHTML += card
+
+  }
+  );
+}
